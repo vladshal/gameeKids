5066b8bab892e9471ee10310c43f584bf8c16692
vlad <shalimov-vlad132@mail.ru>
2019-04-06 14:56:44 +0400

    add command "main"

---
+++ gameeKids.py
@@ -0,0 +1,18 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+from mcpi.minecraft import Vec3
+from minecraftstuff import MinecraftDrawing
+from minecraftstuff import MinecraftShape, ShapeBlock
+from random import randint
+from time import sleep
+mc = minecraft.Minecraft.create()
+mcDrawing = MinecraftDrawing(mc)
+
+
+def main():
+    pass
+
+
+
+main()
+
