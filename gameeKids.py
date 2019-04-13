383f88a7eccee941f572f5db755609e7d006591e
vlad <shalimov-vlad132@mail.ru>
2019-04-13 15:33:36 +0400

    prepare2

--- gameeKids.py
+++ gameeKids.py
@@ -13,8 +13,9 @@ mcDrawing = MinecraftDrawing(mc)
 def prepare():
     mc.setBlocks(0,0,0,15,0,15,block.TNT)
     mc.setBlocks(-1,0,16,16,5,16,block.BEDROCK)
-    mc.setBlocks(-1,0,0,-1,5,0,block.BEDROCK)
-    
+    mc.setBlocks(16,0,-1,16,5,16,block.BEDROCK)
+    mc.setBlocks(-1,0,-1,-1,5,16,block.BEDROCK)
+    mc.setBlocks(0,0,-1,15,5,-1,block.BEDROCK)
 def game():
     pass
         
