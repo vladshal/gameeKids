62ad249b487e0a017cc4c1066f3c132b5ab693c8
vlad <shalimov-vlad132@mail.ru>
2019-04-27 15:35:11 +0400

    END2

--- gameeKids.py
+++ gameeKids.py
@@ -11,6 +11,7 @@ mcDrawing = MinecraftDrawing(mc)
 
 
 def prepare():
+    mc.setBlocks(-100,-100,-30,100,100,100,block.AIR)
     mc.setBlocks(0,0,0,15,0,15,block.GRASS)
     mc.setBlocks(-1,0,16,16,5,16,block.MOSS_STONE)
     mc.setBlocks(16,0,-1,16,5,16,block.MOSS_STONE)
@@ -29,24 +30,25 @@ def prepare():
     mc.setBlocks(-1,-10,-1,-1,-5,16,block.BRICK_BLOCK)
     mc.setBlocks(0,-10,-1,15,-5,-1,block.BRICK_BLOCK)
 
-    mc.setBlocks(0,-15,0,15,-15,15,block.SNOW)
-    mc.setBlocks(-1,-15,16,16,-10,16,block.ICE)
-    mc.setBlocks(16,-15,-1,16,-10,16,block.ICE)
-    mc.setBlocks(-1,-15,-1,-1,-10,16,block.ICE)
-    mc.setBlocks(0,-15,-1,15,-10,-1,block.ICE)
+    mc.setBlocks(0,-15,0,15,-15,15,block.ICE)
+    mc.setBlocks(-1,-15,16,16,-10,16,block.SNOW)
+    mc.setBlocks(16,-15,-1,16,-10,16,block.SNOW)
+    mc.setBlocks(-1,-15,-1,-1,-10,16,block.SNOW)
+    mc.setBlocks(0,-15,-1,15,-10,-1,block.SNOW)
 
-    mc.setBlocks(0,-15,0,15,-15,15,block.GLOWING_OBSIDIAN)
-    mc.setBlocks(-1,-15,16,16,-10,16,block.LAVA)
-    mc.setBlocks(16,-15,-1,16,-10,16,block.LAVA)
-    mc.setBlocks(-1,-15,-1,-1,-10,16,block.LAVA)
-    mc.setBlocks(0,-15,-1,15,-10,-1,block.LAVA)
+    mc.setBlocks(0,-25,0,15,-25,15,block.LEAVES)
+    mc.setBlocks(-1,-25,16,16,-20,16,block.CACTUS)
+    mc.setBlocks(16,-25,-1,16,-20,16,block.CACTUS)
+    mc.setBlocks(-1,-25,-1,-1,-20,16,block.CACTUS)
+    mc.setBlocks(0,-25,-1,15,-20,-1,block.CACTUS)
     
     mc.setBlocks(0,-20,0,15,-20,15,block.STONE)
     mc.setBlocks(-1,-20,16,16,-15,16,block.DIAMOND_ORE)
     mc.setBlocks(16,-20,-1,16,-15,16,block.GOLD_ORE)
     mc.setBlocks(-1,-20,-1,-1,-15,16,block.IRON_ORE )
-    mc.setBlocks(0,-20,-1,15,-15,-1,block.REDSTONE_ORE)
+    mc.setBlocks(0,-20,-1,15,-15,-1,block.COAL_ORE)
     
+  
 def game():
     b = mc.player.getPos()
     sleep(0.1)   
